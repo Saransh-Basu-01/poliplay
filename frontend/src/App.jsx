5b1b41414c34e498ed8514d27ebb27223f29ead2
Saransh-Basu-01 <saranshbasu@gmail.com>
2025-07-25 23:04:56 +0545

    added timeline

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,14 @@ import Home from './components/Home'
 import Leaderboard from './leaderboard/leaderboard'
 import Categories from './games/categories'
 import Timeline from './Resources/Timeline'
+import Documentation from './Resources/Documentation';
+import Chatbot from './Resources/Chatbot';
 import Quiz from './games/Quiz'
 import Scenario from './games/Scenarioplay'
 import Sorting from './games/sorting'
 import CardGame from './games/CardGame';
+import Resources from './Resources/Resources';
+
 const App = () => {
   return (
     <Router>
@@ -20,11 +24,13 @@ const App = () => {
           <Route path="Scenario" element={<Scenario />} />
           <Route path="Sorting" element={<Sorting />} />
         </Route>
-        <Route path="/resources" element={<Timeline />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/resources/timeline" element={<Timeline />} />
+        <Route path="/resources/documentation" element={<Documentation />} />
+        <Route path="/resources/chatbot" element={<Chatbot />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
