03acf951ca5fe7ce196857390f5613186c8e07e2
Prajwal <prajwalsitoula8848@gmail.com>
2025-07-26 08:40:04 +0545

    added

--- backend/poliplay/urls.py
+++ backend/poliplay/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chatbot.urls')),
+     path('api/quiz', include('quize.urls')),
+    # path('api/news', include('Dnews.urls')),
 ]
